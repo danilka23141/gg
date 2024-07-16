25639eab7a184b0988c10364b90f4ba8f28a9d7e
Mikhalev Daniil <mixalyov1994@list.ru>
2024-07-16 13:14:05 +0300

    Редакт11

--- src/main/java/VacationPlannerService.java
+++ src/main/java/VacationPlannerService.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class VacationPlannerService {
+
+
+public class VacationPlannerService {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = money - expenses;
+                money /= 3; // траты на отдых
+            } else {
+                money = money + income;
+                money = money - expenses; // обязательные траты
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/VacationPlannerServiceTest.java
+++ src/test/java/VacationPlannerServiceTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class VacationPlannerServiceTest {
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VacationPlannerServiceTest {
+    @Test
+    public void shouldCalculateVacationMonths() {
+        VacationPlannerService service = new VacationPlannerService();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+        int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
+        assertEquals(expected, actual);
+    }
 }
